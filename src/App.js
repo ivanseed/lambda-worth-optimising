0a7c9700317ec4956e32a668b32c1c01429b87e8
Ivan Seed <ivan.seed@infinityworks.com>
2021-01-25 16:50:34 +0000

    Links to blog post http://localhost:3000/lambda-worth-optimising

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
             <Styled.Heading>
                 How much money will you save optimising your Lambda?
             </Styled.Heading>
+            <Styled.Description>
+                Is it worth micro optimising your Lambda functions for cost and
+                how much money will you realistically save (or lose!)? You can{' '}
+                <Styled.Link href="https://infinityworks.com/insights/is-it-worth-optimising-your-lambda-functions/">
+                    read the full article here
+                </Styled.Link>
+                .
+            </Styled.Description>
             <Calculator />
         </Styled.Container>
     );
--- src/styles.js
+++ src/styles.js
@@ -13,6 +13,18 @@ export const Container = styled.div`
 export const Heading = styled.h1`
     color: #444;
     margin-top: 40px;
-    margin-bottom: 40px;
+    margin-bottom: 30px;
     max-width: 500px;
+`;
+
+export const Description = styled.div`
+    color: #444;
+    max-width: 600px;
+    margin-bottom: 30px;
+    font-size: 16px;
+`;
+
+export const Link = styled.a`
+    color: #9854f7;
+    font-weight: bold;
 `;
